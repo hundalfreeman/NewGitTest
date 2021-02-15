c21557b223954834bfd7ee50855ea72a2333c991
Kulwinder Singh <official.kulwinder@hotmail.com>
2021-02-15 16:40:56 -0500

    Added new functionality
    Added a new function "Ask_drink". Which will ask the customer. whether they would like to have something to drink.

--- test_file_1.py
+++ test_file_1.py
@@ -7,8 +7,11 @@ Creating a function to add greeting with the person's name
 def greet(name):
     return "Good Morning! " + name + " And have a nice day"
 
+def ask_drink(name):
+    return "Hi " + name + ", would like to have something to drink. Either tea or coffee."
 
 
 print(greet("Kulwinder"))
 
+print(ask_drink("Kulwinder"))
 
