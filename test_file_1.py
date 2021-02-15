e352a8588e45c2ff5daf9cb4f8186553f1523467
Kulwinder Singh <official.kulwinder@hotmail.com>
2021-02-15 16:32:15 -0500

    Update test_file_1.py

--- test_file_1.py
+++ test_file_1.py
@@ -5,7 +5,7 @@ Creating a function to add greeting with the person's name
 
 
 def greet(name):
-    return "Good Morning! " + name
+    return "Good Morning! " + name + " And have a nice day"
 
 
 
